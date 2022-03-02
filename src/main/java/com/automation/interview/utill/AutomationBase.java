3bae2dc3f3d5a4775e3323f3c3c31509e39d1866
Dinesh Kumar gandla <dineshkumargandla@gmail.com>
2022-03-02 23:48:13 +0530

    Initial Draft Push

---
+++ src/main/java/com/automation/interview/utill/AutomationBase.java
@@ -0,0 +1,43 @@
+package com.automation.interview.utill;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class AutomationBase {
+	public static WebDriver driver;
+	private StringBuilder screenDir;
+	private int count = 1;
+	public AutomationBase() throws Exception {
+		Config.init();
+	}
+	private String getScreenDir() {
+		if (null == screenDir) {
+			screenDir = new StringBuilder(Config.getScreenOutDir());
+			File dir = new File(screenDir.toString());
+			if (!(dir.exists()))
+				dir.mkdirs();
+		}
+		return screenDir.toString();
+	}
+	
+	private String buildPath(String filename) {
+		StringBuilder sb = new StringBuilder(getScreenDir());
+		sb.append(File.separatorChar).append(buildFilename(filename));
+		return sb.toString();
+	}
+	
+	public void captureScreen(String filename) {
+		String filePath = buildPath(filename);
+		CaptureScreenshot.capture(driver, filePath);
+	}
+	private String buildFilename(String name) {
+		String pName = this.getClass().getName();
+		StringBuilder sb = new StringBuilder(name);
+		sb.append("_").append(pName.substring(pName.lastIndexOf(".") + 1)).append("_").append(count).append(".png");
+		count++;
+		return sb.toString();
+	}
+	
+}
